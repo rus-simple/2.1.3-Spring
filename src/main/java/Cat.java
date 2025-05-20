aa24f0e8524b99e0090d7ccd239e3890ceaf84f4
Sergey <iamrussia@mail.ru>
2025-05-20 17:22:39 +0700

    create class Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    private Cat cat;
+
+    public Cat() {}
+
+    public Cat(Cat cat) {
+        this.cat = cat;
+    }
+    public Cat getCat() {
+        return cat;
+    }
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+}
